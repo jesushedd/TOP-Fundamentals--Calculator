6f10aeffa605e8af89e692f150dd2c84c660a005
jesushedd <jesushe99@gmail.com>
2025-01-27 13:56:04 -0500

    Add feature, only 1 operator button can be pressed at same time

--- scr.js
+++ scr.js
@@ -6,17 +6,33 @@ const subButton = document.querySelector("#sub");
 //input for numbers
 const numsInput = document.querySelector("input");
 
-//add event listener for buttons
-const updatePressed = (e) => {
+//chage state of a button to pressed
+//unpress other buttons , of same type?
+const pressButton = (e) => {
+    //add press state
     const boton = e.target;
-    boton.classList.toggle("pressed"); 
+    if(boton.classList.contains("pressed")){
+        return;
+    }
+    boton.classList.add("pressed");
+    //remove press state from other buttons
+    const otherButtons = Array.from(document.querySelectorAll(".operator"));        
+    otherButtons.splice(otherButtons.indexOf(boton),1);
+    otherButtons.forEach( (b) => {
+            b.classList.remove("pressed");
+    });
 }
-sumButton.addEventListener("click", updatePressed);
-subButton.addEventListener("click", updatePressed);
+//add event listener for buttons
+sumButton.addEventListener("click", pressButton);
+subButton.addEventListener("click", pressButton);
 
-//add event listener to update A or B when input numbers    
+
+
+//
 const flushNum = (e) => {
+    //get number from input field
     let inputNumber = Number(e.target.value);
+    //check buttons
     if (isOperatorPressed()){
         B = inputNumber;
     } else {
@@ -26,21 +42,8 @@ const flushNum = (e) => {
     console.log("A : " + A);
     console.log("B : " + B);
 };
-
+//event listener to update A or B when input numbers    
 numsInput.addEventListener("keyup", flushNum)
-
-
-
-
-
-
-let acOP = false;
-
-if (acOP){
-    B = Number(prompt("Enter A num"));
-} else {
-    A = Number(prompt("Enter A num: "));
-}
 
 
 
