a56e81f678ea666c9abc62e4cc7ac1c324e05e31
jesushedd <jesushe99@gmail.com>
2025-01-28 08:41:03 -0500

    Add fixed size queue for storing operands - The max of queue is 2 -> 2 operands - If a third operand is enqued , execute operation for the previos operands. Then indert the result. Finally insert third operand

--- scr.js
+++ scr.js
@@ -1,13 +1,46 @@
 const MAX_DISPLAY_LENGHT = 8;
-let A=0;
-let B=0;
+
+let currentOperator = "";
+
+//Ds for saving operands
+//Fixed size queue , max size 2
+//if already full, make operation the insert the result
+const OPERANDS_QUEUE = {
+    queue:[],
+    max_size:2,
+    insert: function  (n) {
+        if (this.queue.length >= this.max_size){
+            this.queue.unshift(this.operate());
+        }
+        this.queue.unshift(n);
+    },
+
+    operate: function (){
+        let a = this.queue.pop();
+        let b = this.queue.pop();
+        switch (currentOperator) {
+            case "+":
+                return a + b;
+            case "*":
+                return a * b;
+                
+            case "-":
+                return a - b;
+                
+            case "/":
+                return a / b;
+                        }
+    }
+}
+
+
 //buttons + -
 const sumButton = document.querySelector("#sum");
 const subButton = document.querySelector("#sub");
 //input for numbers
-const display = document.querySelector(".display");
+const DISPLAY = document.querySelector(".display");
 //numbers keys
-const numericKeys = document.querySelector("#numerics");
+const NUMERIC_KEYS = document.querySelector("#numerics");
 
 
 //add event listener for buttons
@@ -15,10 +48,10 @@ sumButton.addEventListener("click", pressButton);
 subButton.addEventListener("click", pressButton);
 
 //event listener to update A or B when input numbers    
-display.addEventListener("keyup", flushNumber);
+DISPLAY.addEventListener("keyup", flushNumber);
 
 //event listener for numeric keys
-numericKeys.addEventListener("click", pressNumber);
+NUMERIC_KEYS.addEventListener("click", pressNumber);
 
 
 
@@ -27,8 +60,17 @@ numericKeys.addEventListener("click", pressNumber);
 //chage state of a button to pressed
 //unpress other buttons , of same type?
 function pressButton (e){
+
+    console.log(OPERANDS_QUEUE);
+
     //add press state
     const boton = e.target;
+    currentOperator = boton.value;
+    //insert value in operands queue
+    const numberInDisplay = Number(DISPLAY.textContent);
+    OPERANDS_QUEUE.insert(numberInDisplay);
+    
+    console.log(OPERANDS_QUEUE);
     if(boton.classList.contains("pressed")){
         return;
     }
@@ -38,7 +80,7 @@ function pressButton (e){
     otherButtons.splice(otherButtons.indexOf(boton),1);
     otherButtons.forEach( (b) => {
             b.classList.remove("pressed");
-    });
+    });    
 }
 
 
@@ -83,13 +125,13 @@ function pressNumber(e) {
     }
     const number = target.value;
     if (!isOperatorPressed()){
-        display.textContent += number;
+        DISPLAY.textContent += number;
         return;
     }
     //clear display
-    display.textContent = "";
+    DISPLAY.textContent = "";
     
-    display.textContent = target.value;
+    DISPLAY.textContent = target.value;
 
 
     //remove operator pressing state
@@ -101,6 +143,6 @@ function pressNumber(e) {
 }
 
 function isDisplayFull(){
-    return display.textContent.length >=MAX_DISPLAY_LENGHT;
+    return DISPLAY.textContent.length >=MAX_DISPLAY_LENGHT;
 }
 
